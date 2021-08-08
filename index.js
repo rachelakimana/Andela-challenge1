34a5b4be19e4cd98b6868e56ffcc70b77c433c64
rachelakimana <rachelakimana@gmail.com>
2021-08-08 13:57:34 +0200

    cars card column are finished without responsiveness

---
+++ index.js
@@ -0,0 +1,12 @@
+const rotimg = (img) => {
+    // img.style.transform ="rotate(90deg)"
+    img.getElementsByTagName('img');
+    img.setAttribute('style', 'transform:rotate(80deg)');
+    
+    
+
+}
+
+const normalimg = (img) => {
+    img.style.padding="2rem 2rem 0rem 2rem";
+}
